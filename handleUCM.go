1979bb18de03a41c0b05dc47583838b5c7e5782c
Parad0xPl <paradxpl@gmail.com>
2019-06-13 20:57:31 +0200

    - small change of name

--- handleUCM.go
+++ handleUCM.go
@@ -7,5 +7,6 @@ import (
 // handleUCM Handle 'User Control Messages'
 func handleUCM(packet Packet, c net.Conn) error {
 	// WHOLE TODO [PAGE 23]
+	// eventType := utils.ReadInt(packet.data.bytes[0:1])
 	return nil
 }
