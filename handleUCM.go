cb20a165be2f3114d4dbdb79f67be403053db195
Parad0xPl <paradxpl@gmail.com>
2019-07-08 14:30:40 +0200

    - UCM: comments have been added

--- handleUCM.go
+++ handleUCM.go
@@ -13,20 +13,33 @@ func handleUCM(packet Packet, c net.Conn) error {
 	switch eventType {
 	case 0:
 		// Stream Begin
+		/*StreamId=*/
+		utils.ReadInt(packet.data.bytes[2:6])
 	case 1:
 		// Stream EOF
+		/*StreamId=*/
+		utils.ReadInt(packet.data.bytes[2:6])
 	case 2:
 		// StreamDry
+		/*StreamId=*/
+		utils.ReadInt(packet.data.bytes[2:6])
 	case 3:
 		// SetBuffer Length
+		/*StreamId=*/
+		utils.ReadInt(packet.data.bytes[2:6])
+		/*BufferLength=*/
+		utils.ReadInt(packet.data.bytes[6:10])
 	case 4:
 		// StreamIs Recorded
+		/*StreamId=*/
+		utils.ReadInt(packet.data.bytes[2:6])
 	case 6:
 		// PingRequest
 		head, body := Create.UCMPingResponse(packet.data.bytes[2:6])
 		sendPacket(c, packet.ctx, head, body)
 	case 7:
 		// PingResponse
+		// Only client
 	}
 
 	return nil
