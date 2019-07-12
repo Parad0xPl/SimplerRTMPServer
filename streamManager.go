bc82bb8fb259dfd5a182182fd9896361b9d91abd
Parad0xPl <paradxpl@gmail.com>
2019-07-12 17:06:20 +0200

    - streamManager:   - simple managment of channels has been added

--- streamManager.go
+++ streamManager.go
@@ -5,11 +5,49 @@ import "errors"
 // StreamObject represent stream
 type StreamObject struct{}
 
+// ChannelObject represent stream
+type ChannelObject struct {
+	name       string
+	key        string
+	subscribed map[string]*ConnContext
+	metadata   map[string]interface{}
+}
+
+func (co *ChannelObject) subscribe(ctx *ConnContext) {
+	// TODO name of subsriber generated from context
+	co.subscribed["test"] = ctx
+}
+
+func (co *ChannelObject) unsubscribe(ctx *ConnContext) {
+	// TODO name of subsriber generated from context
+	delete(co.subscribed, "test")
+}
+
+func (co *ChannelObject) verify(key string) bool {
+	return co.key == key
+}
+
 // StreamManager Object which manages streams
 type StreamManager struct {
 	topFreeID uint
 	db        map[uint]StreamObject
+	channels  map[string]*ChannelObject
 	published map[string]uint
+}
+
+func (sm *StreamManager) checkChannel(name string) bool {
+	_, ok := sm.channels[name]
+	return ok
+}
+
+func (sm *StreamManager) addChannel(name, key string) {
+	if sm.checkChannel(name) == false {
+		sm.channels[name] = &ChannelObject{
+			name:       name,
+			key:        key,
+			subscribed: make(map[string]*ConnContext),
+		}
+	}
 }
 
 func (sm *StreamManager) createStream() uint {
@@ -58,4 +96,8 @@ func initStrMan() {
 	}
 	streamsMan.db = make(map[uint]StreamObject)
 	streamsMan.published = make(map[string]uint)
+	streamsMan.channels = make(map[string]*ChannelObject)
+
+	// Temporary static channel
+	streamsMan.addChannel("ksawk", "keyfortheapp")
 }
