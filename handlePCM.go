802dff339088a1ba6b77c34bcc72ef8fb99f96dc
Parad0xPl <paradxpl@gmail.com>
2019-07-12 16:21:43 +0200

    - refactor of PCM handler

--- handlePCM.go
+++ handlePCM.go
@@ -4,15 +4,14 @@ import (
 	"SimpleRTMPServer/utils"
 	"errors"
 	"fmt"
-	"log"
 )
 
 // handlePCM Handle 'Protocol Control Messages'
 func handlePCM(packet ReceivedPacket) error {
-	log.Println("PCM Packet")
 	head := packet.header
 	ctx := packet.ctx
-	if head.TypeID == 1 {
+	switch head.TypeID {
+	case 1:
 		fmt.Println("Get 'Set Chunk Size'")
 		var patt byte = 1
 		patt <<= 7
@@ -20,18 +19,18 @@ func handlePCM(packet ReceivedPacket) error {
 			return errors.New("Wrong chunk size")
 		}
 		ctx.ChunkSize = utils.ReadInt(packet.data)
-	} else if head.TypeID == 2 {
+	case 2:
 		fmt.Println("Get 'Abort Message'")
 		// TODO Abort message
-	} else if head.TypeID == 3 {
+	case 3:
 		fmt.Println("Get 'Acknowledgement'")
 		seqnum := utils.ReadInt(packet.data[0:4])
 		fmt.Println("Sequence number:", seqnum)
-	} else if head.TypeID == 5 {
+	case 5:
 		fmt.Println("Get 'Window Acknowledgement Size'")
 		packet.ctx.ClientWindowAcknowledgement = utils.ReadInt(packet.data[0:4])
 		fmt.Println("Client WinAck:", packet.ctx.ClientWindowAcknowledgement)
-	} else if head.TypeID == 6 {
+	case 6:
 		fmt.Println("Get 'Set Peer Bandwidth'")
 		typ := utils.ReadInt(packet.data[4:5])
 		if !(typ == 2 && packet.ctx.PeerBandwidthType == 1) {
